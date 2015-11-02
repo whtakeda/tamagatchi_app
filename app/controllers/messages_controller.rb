ed9f9a351d5d8ee22e3f466ed79ef41fedb947cc
Wayne Takeda <whtakeda@gmail.com>
2015-11-01 22:47:24 -0800

    allowing new posts to be added to message board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,25 @@
-class MessageController < ApplicationController
-  belongs_to :user
+class MessagesController < ApplicationController
   def index
     @messages = Message.all.order(id: :desc)
   end
+
+  def new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.length = @message.body.length
+    @message.user_id = current_user.id
+    if @message.save
+      redirect_to tamagatchis_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Implement Strong Params
+    def message_params
+      params.require(:message).permit(:subject, :body)
+    end
 end
