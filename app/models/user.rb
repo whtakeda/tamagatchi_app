f70a730e4ed4f3e8b7ce324b1fef93eee3e3e11e
Wayne Takeda <whtakeda@gmail.com>
2015-11-05 17:55:13 -0800

    Fixing edit profile bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
  has_secure_password
  # Verify that an email exists and that it does not already exist in the db
  validates :email, presence: true, uniqueness: true
- validates :password, length: { minimum: 4 }
+ validates :password, length: { minimum: 4 }, allow_nil:true
 
  has_many :messages, :dependent => :destroy
  has_one :tamagatchi
